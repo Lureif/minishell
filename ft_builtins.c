af74554dfd9e4c4dfde9b0f93aa460ee9ba3522d
Lureif <lureif.me@gmail.com>
2019-11-16 08:01:20 +0100

    little cheeky test

--- ft_builtins.c
+++ ft_builtins.c
@@ -12,11 +12,46 @@
 
 #include "minishell.h"
 
+char	*ft_builtin_names[__BUILTIN_NUMBER] = {
+	STRINGIFY(ft_cd),
+	STRINGIFY(ft_echo),
+	STRINGIFY(ft_help),
+};
+
+bool	(*ft_builtin_functions[__BUILTIN_NUMBER])(char **) = {
+	&ft_cd,
+	&ft_echo,
+	&ft_help
+};
+
 bool	ft_cd(char *dir)
 {	
-	if(chdir(dir) != 0)
+	if (chdir(dir) != 0)
 	{
 		return (false);
 	}
 	return (true);
+}
+
+bool	ft_echo(char *str)
+{
+	ft_putstr_fd(str, 1);
+	return (true);
+}
+
+bool	ft_help(void)
+{
+	unsigned short i;
+
+	i = 0;
+	ft_putstr("Minishell: the (very) basic terminal emulator.\n");
+	ft_putstr("Usage: [Unimaginative Prompt] : [COMMAND] [ARGUMENTS]\n");
+	ft_putstr("builtins:\n");
+	while (i < __BUILTIN_NUMBER)
+	{
+		ft_putstr("\t->");
+		ft_putstr(ft_builtin_names[i]);
+		ft_putchar('\n');
+		i++;
+	}
 }
--- minishell.h
+++ minishell.h
@@ -7,6 +7,7 @@
 #include "./libft/libft.h"
 #include "./get_next_line.h"
 #define SH_PROMPT "[Unimaginative prompt] : "
+#define STRINGIFY(x) #x
 #define TOKEN_MAX 255
 
 typedef struct		s_env
@@ -14,7 +15,35 @@ typedef struct		s_env
 	char *PATH;
 }					t_env;
 
-int		ret_env_var_index(char **env, char *var, int initial_pos);
+typedef	enum		s_builtin_enum
+{
+	ft_cd,
+	ft_echo,
+	ft_help,
+	__BUILTIN_NUMBER
+}			t_builtin_enum;
+
+/*
+**	Environement related functions
+*/
+
+int	ret_env_var_index(char **env, char *var, int initial_pos);
 void	ret_env_var(char **env_var_name, int var_len);
+
+/*
+**	Memory management functions
+*/
 void	ft_free_d_tab(void **d_tab);
+
+/*
+**	Builtin functions
+*/
+
+bool	ft_cd(char *dir);
+bool	ft_echo(char *str);
+bool	ft_help(void);
+
+/*
+**	Main functions
+*/
 void	ft_print_prompt(void);
